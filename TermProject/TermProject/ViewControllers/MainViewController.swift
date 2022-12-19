5328ec6ed84ccb86ad0512f8ae9b8e29be2a0f60
Serkan Berk Bilgiç <sberk@Serkan-MacBook-Pro.local>
2022-12-20 00:03:17 +0300

    first commit initialization of project

---
+++ TermProject/TermProject/ViewControllers/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TermProject
+//
+//  Created by Serkan Berk Bilgiç on 19.12.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
