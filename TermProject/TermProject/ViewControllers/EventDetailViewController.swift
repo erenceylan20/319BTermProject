539f2c12688cbf6933ad7fab0a62ba33c9a7aba8
erenceylan20 <erenceylan20@ku.edu.tr>
2022-12-21 16:59:12 +0300

    Event Detail Screen initialized

---
+++ TermProject/TermProject/ViewControllers/EventDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EventDetailViewController.swift
+//  TermProject
+//
+//  Created by Eren Ceylan on 21.12.2022.
+//
+
+import UIKit
+
+class EventDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
