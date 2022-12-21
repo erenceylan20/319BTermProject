e8c33b4ab7da3640e225499788b91d4c3c86283a
erenceylan20 <erenceylan20@ku.edu.tr>
2022-12-21 14:24:05 +0300

    initializedHomeScreen

--- TermProject/TermProject/ViewControllers/MainViewController.swift
+++ TermProject/TermProject/ViewControllers/MainViewController.swift
@@ -9,13 +9,37 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    
+    @IBOutlet weak var eventListTableView: UITableView!
+    
     @IBOutlet weak var testLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        testLabel.text = "Text has changed"
+        testLabel.text = "New Text"
+        self.title = "Events"
     }
 
 
 }
 
+extension MainViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "EventCell") as? EventTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        return cell
+    }
+    
+    
+}
