2743ca7db773908725b48134e0147e74fc6729d5
erenceylan20 <erenceylan20@ku.edu.tr>
2023-01-02 01:43:28 +0300

    Course screen added

---
+++ TermProject/TermProject/Models/CourseDataSource.swift
@@ -0,0 +1,80 @@
+//
+//  CourseDataSource.swift
+//  TermProject
+//
+//  Created by Eren Ceylan on 2.01.2023.
+//
+
+import Foundation
+
+
+class CourseDataSource {
+    
+    let weekDays: [String] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    
+    private var courseArray: [[Course]] = [
+        [
+        Course(id: "1", title: "Linear Algebra", code: "ENGR200", beginningTime: Date(), endingTime: Date()),
+        Course(id: "1", title: "Software Engineering", code: "COMP302", beginningTime: Date(), endingTime: Date()),
+        Course(id: "1", title: "Mobile App Develpoment", code: "COMP319B", beginningTime: Date(), endingTime: Date()),
+        Course(id: "1", title: "Managerial Accounting", code: "ACCT202", beginningTime: Date(), endingTime: Date()),
+        ],
+        [
+        Course(id: "1", title: "Linear Algebra", code: "ENGR200", beginningTime: Date(), endingTime: Date()),
+        Course(id: "1", title: "Software Engineering", code: "COMP302", beginningTime: Date(), endingTime: Date()),
+        Course(id: "1", title: "Mobile App Develpoment", code: "COMP319B", beginningTime: Date(), endingTime: Date()),
+        ],
+        [
+        Course(id: "1", title: "Linear Algebra", code: "ENGR200", beginningTime: Date(), endingTime: Date()),
+        Course(id: "1", title: "Software Engineering", code: "COMP302", beginningTime: Date(), endingTime: Date()),
+        Course(id: "1", title: "Mobile App Develpoment", code: "COMP319B", beginningTime: Date(), endingTime: Date()),
+        Course(id: "1", title: "Managerial Accounting", code: "ACCT202", beginningTime: Date(), endingTime: Date()),
+        ],
+        [
+        Course(id: "1", title: "Linear Algebra", code: "ENGR200", beginningTime: Date(), endingTime: Date()),
+        Course(id: "1", title: "Software Engineering", code: "COMP302", beginningTime: Date(), endingTime: Date()),
+        Course(id: "1", title: "Mobile App Develpoment", code: "COMP319B", beginningTime: Date(), endingTime: Date()),
+        ],
+        [],
+        [],
+        []
+
+        ]
+    //private let baseURL = "https://wizard-world-api.herokuapp.com"
+    //var delegate: CourseDataDelegate?
+    
+    init() {
+        
+        
+    }
+    
+    
+    func getNumberOfCourses() -> Int {
+        var tot = 0
+        for array in courseArray {
+            tot += array.count
+        }
+        return tot
+    }
+    
+    func getNumberOfCourses(day: Int) -> Int {
+        return courseArray[day].count
+    }
+    
+    func getWeekDayName(day: Int) -> String {
+        return weekDays[day]
+    }
+
+    
+    func getCourse(day: Int, index:Int) -> Course? {
+        guard day < courseArray.count else {
+            return nil
+        }
+        guard index < courseArray[day].count else {
+            return nil
+        }
+        
+        return courseArray[day][index]
+    }
+    
+}
--- TermProject/TermProject/ViewControllers/CoursesViewController.swift
+++ TermProject/TermProject/ViewControllers/CoursesViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class CoursesViewController: UIViewController {
+    
+    @IBOutlet weak var courseListTableView: UITableView!
+    
+    let courseDataSource = CourseDataSource()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,4 +30,49 @@ class CoursesViewController: UIViewController {
     }
     */
 
+}
+
+extension CoursesViewController: UITableViewDataSource {
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 7
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return courseDataSource.getNumberOfCourses(day: section)
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "CourseCell") as? CourseTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        
+        if let course = courseDataSource.getCourse(day: indexPath.section, index: indexPath.row) {
+            cell.courseLabel.text = "\(course.title)"
+            
+            let formatter = DateFormatter()
+            formatter.dateFormat = "HH:mm"
+            let beginningTime = formatter.string(from: course.beginningTime)
+            let endingTime = formatter.string(from: course.endingTime)
+            
+            cell.timeLabel.text = "\(beginningTime) - \(endingTime)"
+            
+        
+        } else {
+            
+        }
+        
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return courseDataSource.getWeekDayName(day: section)
+    }
+    
+    
 }
