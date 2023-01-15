d61394d4ed7bb18ec5ddec8719bee29de73f247e
erenceylan20 <erenceylan20@ku.edu.tr>
2023-01-15 21:49:54 +0300

    Setting screen added with auto layout

---
+++ TermProject/TermProject/ViewControllers/SettingsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SettingsViewController.swift
+//  TermProject
+//
+//  Created by Eren Ceylan on 15.01.2023.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var surnameTextField: UITextField!
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
